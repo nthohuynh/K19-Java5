eaf03f34b447e64f87a72d771fdc698b17fea050
tthanh19ce <dtthanh.19ce@gmail.com>
2019-09-07 16:19:50 +0700

    Create Ptbac1.java

---
+++ DinhTanThanh19CE/baitap1/src/baitap1/Ptbac1.java
@@ -0,0 +1,31 @@
+package baitap1;
+
+import java.util.Scanner;
+
+public class Ptbac1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float a,b,x;
+		System.out.println("nhap a= ");
+		Scanner scn = new Scanner(System.in);
+		a= scn.nextFloat();
+		System.out.println("nhap b= ");
+		b= scn.nextFloat();
+		if (a==0) {
+			if (b==0) {
+				System.out.println("phuong trinh co vo so nghiem");
+			}
+			if (b!=0) {
+				System.out.println("phuong trinh vo so nghiem");
+			}
+				
+		}
+		if (a!=0) {
+			x = -b/a;
+			System.out.println("phuong trinh co nghiem x= "+x);
+		}
+
+	}
+
+}
